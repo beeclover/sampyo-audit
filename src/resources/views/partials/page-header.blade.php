741c119be0bc9a59ee12156aa9cd0d4148d88808
Hansanghyeon <999@hyeon.pro>
2021-12-08 13:55:25 +0900

    :sparkles: page header
    Resolves: #5

--- src/resources/views/partials/page-header.blade.php
+++ src/resources/views/partials/page-header.blade.php
@@ -1,3 +1,8 @@
-<div class="page-header">
-  <h1>{!! $title !!}</h1>
+<div class="relative max-w-[1920px] container h-[350px] overflow-hidden">
+  <div class="absolute top-1/2 left-1/2 -translate-x-1/2 text-white text-center z-10">
+    <div class="page-header">
+      <h1 class="text-[50px] font-black">{!! $title !!}</h1>
+    </div>
+  </div>
+  @include('partials.img', ['ex' => 'jpg', 'name' => 'header-01', 'class' => 'absolute top-0 left-0 w-full h-full object-cover'])
 </div>
