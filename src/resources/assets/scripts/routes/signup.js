94e0b242b4d4579c11acd9ab5ea210c6ecdbf04c
Hansanghyeon <999@hyeon.pro>
2021-12-14 01:31:07 +0900

    :construction: signup ui

---
+++ src/resources/assets/scripts/routes/signup.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue';
+import Form from '../components/Form/Signup';
+
+const common = {
+  containerInit() {},
+  init() {
+    const app = createApp({});
+    app.component('vue-form', Form).mount('#form');
+  },
+  finalize() {
+    // JavaScript to be fired on all pages, after page specific JS is fired
+  },
+};
+
+export default common;
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -4,6 +4,21 @@ const colorReset = {
   color: null,
 };
 
+const colors = {
+  lochmara: {
+    50: '#f4f8fc',
+    100: '#e8f1f9',
+    200: '#c6dcf0',
+    300: '#a3c7e7',
+    400: '#5f9dd6',
+    500: '#1a73c4',
+    600: '#1768b0',
+    700: '#145693',
+    800: '#104576',
+    900: '#0d3860',
+  },
+};
+
 module.exports = {
   mode: 'jit',
   purge: {
@@ -16,7 +31,7 @@ module.exports = {
       padding: 'var(--container-px)',
     },
     extend: {
-      colors: {},
+      colors,
       fontFamily: {
         sans: ['"Noto Sans KR"', ...defaultTheme.fontFamily.sans],
       },
