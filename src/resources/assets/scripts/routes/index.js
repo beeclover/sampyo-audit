a9834ada6095e37f5bd7ed56ccc9cf4062a2dd19
Hansanghyeon <999@hyeon.pro>
2021-12-17 17:37:45 +0900

    :truck: rename create Report

--- src/resources/assets/scripts/routes/index.js
+++ src/resources/assets/scripts/routes/index.js
@@ -1,9 +1,9 @@
 import common from './common';
-import report from './report';
+import createReport from './createReport';
 import signup from './signup';
 
 export default {
   common,
-  createReport: report,
+  createReport,
   signup,
 };
