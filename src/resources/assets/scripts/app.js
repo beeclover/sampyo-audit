3853b90481373263d63b2b5cd180e27fdfd993a8
Hansanghyeon <999@hyeon.pro>
2021-12-23 13:29:16 +0900

    :construction: mobile menu open status

--- src/resources/assets/scripts/app.js
+++ src/resources/assets/scripts/app.js
@@ -1,6 +1,7 @@
 require('./bootstrap');
 import 'jquery';
 import Alpine from 'alpinejs';
+import { bodyScrollLock } from './common/alpinejs';
 
 import Router from './util/router';
 import route from './routes';
@@ -9,5 +10,12 @@ const routes = new Router(route);
 
 jQuery(document).ready(() => {
   routes.loadEvents();
+  Alpine.store('mobileMenu', {
+    on: false,
+    toggle() {
+      this.on = !this.on;
+      bodyScrollLock(this.on);
+    },
+  });
   Alpine.start();
 });
