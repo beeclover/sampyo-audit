2b37f273a3fe367896d92c898b7cb1b30f96bfb2
Hansanghyeon <999@hyeon.pro>
2021-12-23 13:54:34 +0900

    :construction: FNB

--- src/resources/views/partials/footer.blade.php
+++ src/resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
-<footer class="footer__main py-[52px] bg-[#222222] text-white">
+<footer class="footer__main py-[28px] lg:py-[52px] bg-[#222222] text-white text-[0.86rem] font-thin lg:font-normal">
   <div class="container max-w-[1920px] xl:px-[50px]">
-    <ul class="listDivider__cols1">
+    <ul class="lg:listDivider__cols1">
       <li>삼표그룹 감사팀</li>
       <li>서울특별시 종로구 종로1길 42 (수송동) 이마빌딩 6층</li>
       <li>대표전화 02-6270-0765</li>
