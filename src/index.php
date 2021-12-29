4b4ccce16c55031059da04329e04d875a30de8a2
999 <999@hyeon.pro>
2021-12-29 13:16:45 +0900

    :sparkles: 2021-12-29 수정사항 (beeclover/sampyo-audit#31)

--- src/index.php
+++ src/index.php
@@ -8,6 +8,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
     <!-- END Fonts google -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
     <?php wp_head(); ?>
   </head>
 
