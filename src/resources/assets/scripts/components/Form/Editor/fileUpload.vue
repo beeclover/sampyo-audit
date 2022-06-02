cb9a2b00e245f5f7f85c9d1d89921d842cb53700
Hansanghyeon <999@hyeon.pro>
2022-06-02 17:05:35 +0900

    feat: fileUpload.vue code format

--- src/resources/assets/scripts/components/Form/Editor/fileUpload.vue
+++ src/resources/assets/scripts/components/Form/Editor/fileUpload.vue
@@ -1,10 +1,32 @@
 <template>
   <div :id="'file-' + this.index" class="lg:hidden">
     <div class="inputWrap flex items-center gap-x-[13px]">
-      <input hidden type="file" :name="'file-' + this.index" accept=".png, .jpg, .jpge, .zip, .pdf, .pptx, .ppt, .xlsx, .webp">
+      <input
+        hidden
+        type="file"
+        :name="'file-' + this.index"
+        accept=".png, .jpg, .jpge, .zip, .pdf, .pptx, .ppt, .xlsx, .webp"
+      />
       <div>{{ label }}</div>
-      <button v-if="label" @click.prevent="this.remove" aria-label="remove" class="w-[24px] h-[24px] border border-[#707070] rounded-[2px] bg-white">
-        <svg class="w-full h-full fill-[#707070]" id="Layer_3" height="512" viewBox="0 0 32 32" width="512" xmlns="http://www.w3.org/2000/svg" data-name="Layer 3"><path d="m17.414 16 6.293-6.293a1 1 0 0 0 -1.414-1.414l-6.293 6.293-6.293-6.293a1 1 0 0 0 -1.414 1.414l6.293 6.293-6.293 6.293a1 1 0 1 0 1.414 1.414l6.293-6.293 6.293 6.293a1 1 0 0 0 1.414-1.414z"/></svg>
+      <button
+        v-if="label"
+        @click.prevent="this.remove"
+        aria-label="remove"
+        class="w-[24px] h-[24px] border border-[#707070] rounded-[2px] bg-white"
+      >
+        <svg
+          class="w-full h-full fill-[#707070]"
+          id="Layer_3"
+          height="512"
+          viewBox="0 0 32 32"
+          width="512"
+          xmlns="http://www.w3.org/2000/svg"
+          data-name="Layer 3"
+        >
+          <path
+            d="m17.414 16 6.293-6.293a1 1 0 0 0 -1.414-1.414l-6.293 6.293-6.293-6.293a1 1 0 0 0 -1.414 1.414l6.293 6.293-6.293 6.293a1 1 0 1 0 1.414 1.414l6.293-6.293 6.293 6.293a1 1 0 0 0 1.414-1.414z"
+          />
+        </svg>
       </button>
     </div>
   </div>
@@ -21,10 +43,10 @@ export default {
   },
   data() {
     return {
-      label: null
-    }
+      label: null,
+    };
   },
-  mounted () {
+  mounted() {
     const el = this.$el;
     const input = el.querySelector('input');
     document.body.onfocus = () => {
@@ -43,8 +65,8 @@ export default {
       }, 1000);
       // reset
       document.body.onfocus = null;
-    }
-    
+    };
+
     const { sUsrAg } = checkbrowser();
     if (sUsrAg.indexOf('iPhone') !== -1) {
       input.addEventListener('change', () => {
@@ -58,5 +80,5 @@ export default {
 
     input.click();
   },
-}
+};
 </script>
